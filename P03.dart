0364eff2d5bde0af55cc7095efd61fbc13b5c5a2
Nova Eliza Maharani <novaelzrayya@gmail.com>
2025-09-06 11:58:19 +0700

    P03 - if else

---
+++ P03.dart
@@ -0,0 +1,19 @@
+void main() {
+    String test = "test2";
+  if (test == "test1") {
+    print("Test1");
+  } else if (test == "test2") {
+    print("Test2");
+  } else {
+    print("Something else");
+  }
+
+  if (test == "test2") print("Test2 again");
+
+  String testing = "true";
+  if (testing == "true") {
+    print("Kebenaran");
+  } else {
+    print("Bukan kebenaran");
+  }
+}
