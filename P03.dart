2aa1e5637cb51d2114e089255e069c1cce2f5d33
Nova Eliza Maharani <novaelzrayya@gmail.com>
2025-09-06 12:33:43 +0700

    P03 - break, continue

--- P03.dart
+++ P03.dart
@@ -18,15 +18,24 @@ void main() {
   } */
 
   //harus mendeklarasikan dan inisialisasi
-  int counter = 40;
+  /*int counter = 40;
 
   do {
     print(counter);
     counter++;
   } while (counter < 77);
-  
+
   while (counter < 33) {
     print(counter);
     counter++;
+  }*/
+
+  for (int index = 10; index < 27; index++) {
+    if (index == 21) {
+      break; // stop loop di angka 21
+    } else if (index > 1 && index < 7) {
+      continue; // skip angka 2–6 (meski di sini tidak kepakai, karena loop mulai dari 10)
+    } 
+    print(index);
   }
 }
