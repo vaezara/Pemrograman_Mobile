d614c3498d9a63a7b6010cc931340b0daeb09f3f
Nova Eliza Maharani <novaelzrayya@gmail.com>
2025-10-04 20:55:57 +0700

    J5 - 1

---
+++ Praktikum5/layout_flutter/README.md
@@ -0,0 +1,13 @@
+# layout_flutter
+
+Nova Eliza Maharani
+2341720252 / TI 3D - 24
+
+## JOBSHEET PERTEMUAN 6 
+
+#### Praktikum 1
+Implementasi title row
+![1](images/1.png)
+
+#### Praktikum 2
+Implementasi button row
---
+++ Praktikum5/layout_flutter/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Langkah 4: Implementasi title row
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            // soal 1
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // soal 2
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8), 
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(
+                    color: Colors.grey[500], 
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // soal 3
+          const Icon(
+            Icons.star,
+            color: Colors.red, 
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+    return MaterialApp(
+      title: 'Flutter layout: Nova Eliza Maharani, 2341720252 ',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: Column(
+          children: [
+            titleSection,
+          ],
+        ),
+      ),
+    );
+  }
+}
