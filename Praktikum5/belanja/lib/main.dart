07eee56eaf4f6f305b51ad2e07ffd789d26f74bf
Nova Eliza Maharani <novaelzrayya@gmail.com>
2025-10-04 22:05:58 +0700

    J5 - 5

---
+++ Praktikum5/belanja/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+import 'pages/item_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => HomePage(),
+      '/item': (context) => const ItemPage(),
+    },
+  ));
+}
