7672fcaa0f383e055add84c5b4d46998de897077
Nova Eliza Maharani <novaelzrayya@gmail.com>
2025-10-11 22:02:11 +0700

    P06 - langkah 4

---
+++ Praktikum6/flutter_plugin_pubdev/README.md
@@ -0,0 +1,10 @@
+# flutter_plugin_pubdev
+
+A new Flutter project.
+
+## JOBSHEET 6
+
+#### LANGKAH 4
+Error muncul karena kode memanggil widget AutoSizeText dan variabel text tanpa deklarasi dan impor yang benar. Flutter juga tidak mengenali text karena belum ada variabel atau parameter dengan nama tersebut di dalam kelas
+![1](images/1.png)
+
---
+++ Praktikum6/flutter_plugin_pubdev/lib/red_text_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RedTextWidget extends StatelessWidget {
+  const RedTextWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AutoSizeText(
+      text,
+      style: const TextStyle(color: Colors.red, fontSize: 14),
+      maxLines: 2,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+}
